7e1f0fabb159e32d24e46008706f07b76de36e76
matontim <linniesage@gmail.com>
2026-06-27 14:17:41 -0400

    Strings and Lists

---
+++ python-village/strings_and_lists.py
@@ -0,0 +1,9 @@
+Python 3.14.2 (v3.14.2:df793163d58, Dec  5 2025, 12:18:06) [Clang 16.0.0 (clang-1600.0.26.6)] on darwin
+Enter "help" below or click "Help" above for more information.
+>>> string = 'cVpi2Gec8jGTQVuHDql8Zk8c5nSvOB1wnkqTtEtOq6v8I73YnI5kPxOKd6uKQzTPjyfZcyPxr8DQTestudocD3ebs1Cm6itwLVk1n7B3euqRXQMHdsnzwjyU9PffTfAHbqHRYtyrDcpE1hKlwGcJR0EXzASayHuvpBXehrlpB8MMBTMhhezCXzEgripariab.'
+>>> a = string[76:83]
+>>> b = string[184:191]
+>>> print(a + b)
+Testudoriparia
+>>> print(a + ' ' + b)
+Testudo riparia
